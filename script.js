d919efc84acce51abad583165f4aec7cfacc33be
Nomaan Rja <nomaanrja@gmail.com>
2024-01-30 15:54:17 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let button = document.querySelectorAll(".button")
+let body = document.querySelector("body")
+
+button.forEach( function(butt){
+   butt.addEventListener("click" ,function(e){
+    console.log("hello i am working")
+    console.log(e.target)
+    if(e.target.id === 'grey'){
+        body.style.background ="grey"
+    }
+    else if(e.target.id === 'white'){
+        body.style.background ="white"
+    }
+   else if(e.target.id === 'blue'){
+        body.style.background ="blue"
+    }
+    else if(e.target.id === 'yellow'){
+        body.style.background ="yellow"
+    }
+    
+   } )
+})
